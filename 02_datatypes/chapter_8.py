145878e721c5e6148e400462cca2b6960f4473fd
tarun1sisodia <coccoder999@gmail.com>
2025-12-30 09:59:50 +0530

    learn List

---
+++ 02_datatypes/chapter_8.py
@@ -0,0 +1,35 @@
+
+ingredients = [ "water", "milk", "black_tea"]
+ingredients.append("sugar") #adds in last of array.
+print(f"Total Ingredients: {ingredients}")
+
+ingredients.pop()
+print(f"After Poping the last element/ Data : {ingredients}")
+
+ingredients.remove('water')
+
+print(f"After Water: {ingredients}")
+
+# print(f"Ingredients: {ingredients.reverse()}")
+
+spice_options = ["ginger", "Cardmom"]
+chai_ingredients = ["water","milk"]
+
+chai_ingredients.extend(spice_options)
+print(f"Chai and Chai Ingredients : {chai_ingredients}")
+
+chai_ingredients.insert(2,"black_tea")
+print(f"chai:{chai_ingredients}")
+
+last_option = chai_ingredients.pop()
+print(last_option)
+
+chai_ingredients.reverse()
+print(chai_ingredients)
+
+chai_ingredients.sort()
+print(chai_ingredients)
+
+sugar_levels = [1,2,3,4,5]
+print(f"Max of Sugar Level: {max(sugar_levels)}")
+print(f"Min of Sugar Level: {min(sugar_levels)}")
