e8f1bb63f2ab300800b491a9658d80e6f9ef510a
Tarun Sisodia <128500527+tarun1sisodia@users.noreply.github.com>
2026-04-12 09:34:34 +0000

    feat: add initial ticket info system for railway app not done yet

---
+++ 03_Conditionals/train_seat_info.py
@@ -0,0 +1,7 @@
+#You're building a ticket info system for a railway app
+#Based on seat type, show its features
+ #Task:
+    #input: "sleeper", "AC", "general","luxury"
+    #Match using match-case
+    #Unknown -> show: "Invalid seat type"
+
