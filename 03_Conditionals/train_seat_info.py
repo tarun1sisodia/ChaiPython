ef321966030cab028068d030951a620e57fa1a89
Tarun Sisodia <128500527+tarun1sisodia@users.noreply.github.com>
2026-04-15 08:50:29 +0000

    code:Udemy Full stack y Hitesh sir

--- 03_Conditionals/train_seat_info.py
+++ 03_Conditionals/train_seat_info.py
@@ -5,3 +5,15 @@
     #Match using match-case
     #Unknown -> show: "Invalid seat type"
 
+seat = input("Enter your Seat Type to get details: \n").lower()
+
+match seat:
+   case "sleeper":
+      print("This includes Non-Ac and General Seat Quality: \n")
+   case "ac":
+      print("This includes Ac and better quality than all of seat:\n")
+   case "general":
+         print("This doesn't include Seat and and no AC :\n")
+   case "luxury":
+         print("Welcome! Best AC and Seat Availability :\n")
+
